9ad6fddb0010fd7bb6ab3bdb18a150d39490dde0
omarperacha <omar.peracha@gmail.com>
2019-02-12 17:21:37 +0000

    Initial Commit

---
+++ impuls_MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  impuls_MacOS
+//
+//  Created by Omar Peracha on 12/02/2019.
+//  Copyright © 2019 Omar Peracha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
